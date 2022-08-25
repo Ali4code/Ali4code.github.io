a7831436ab975a3e02a7f8218ee8d7197fd2f468
Ali Hoseini <Ali4bcup@gmail.com>
2022-08-26 01:10:07 +0430

    added commits api res interface

---
+++ src/api/interface-commit-respose.ts
@@ -0,0 +1,78 @@
+export interface ICommitResponse {
+  url: string;
+  sha: string;
+  node_id: string;
+  html_url: string;
+  comments_url: string;
+  commit: {
+    url: string;
+    author: {
+      name: string;
+      email: string;
+      date: Date;
+    };
+    committer: {
+      name: string;
+      email: string;
+      date: Date;
+    };
+    message: string;
+    tree: {
+      url: string;
+      sha: string;
+    };
+    comment_count: number;
+    verification: {
+      verified: boolean;
+      reason: string;
+      signature: null;
+      payload: null;
+    };
+  };
+  author: {
+    login: string;
+    id: number;
+    node_id: string;
+    avatar_url: string;
+    gravatar_id: number;
+    url: string;
+    html_url: string;
+    followers_url: string;
+    following_url: string;
+    gists_url: string;
+    starred_url: string;
+    subscriptions_url: string;
+    organizations_url: string;
+    repos_url: string;
+    events_url: string;
+    received_events_url: string;
+    type: string;
+    site_admin: boolean;
+  };
+  committer: {
+    login: string;
+    id: number;
+    node_id: string;
+    avatar_url: string;
+    gravatar_id: number;
+    url: string;
+    html_url: string;
+    followers_url: string;
+    following_url: string;
+    gists_url: string;
+    starred_url: string;
+    subscriptions_url: string;
+    organizations_url: string;
+    repos_url: string;
+    events_url: string;
+    received_events_url: string;
+    type: string;
+    site_admin: boolean;
+  };
+  parents: [
+    {
+      url: string;
+      sha: string;
+    }
+  ];
+}
