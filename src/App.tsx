d5237e2f581c4f98045867c005e49e1bad271cbf
Ali Hoseini <Ali4bcup@gmail.com>
2022-08-26 01:10:27 +0430

    Made app functional and show commits

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,29 @@
 import { useEffect, useState } from "react";
 import getGitHistory from "./api/git-history-api";
-import SingleCommit from "./components/single-commit";
+import SingleCommit from "./components/single commit/single-commit";
 import AuthKeyForm from "./components/auth-key-form";
+import { ICommitResponse } from "./api/interface-commit-respose";
 
 function App() {
-  const [commitsHistory, setCommitsHistory] = useState([]);
+  const [commitsHistory, setCommitsHistory] = useState<ICommitResponse[]>([]);
   useEffect(() => {
     const authKey = localStorage.getItem("auth-key");
     if (!authKey) return;
 
     getGitHistory(authKey).then((data) => {
-      console.log(data);
+      setCommitsHistory(data);
+      console.log(data)
     });
   }, []);
 
-  const time = new Date("2011-04-14T16:00:49Z");
+  const listOfCommits = commitsHistory?.map((item) => {
+    return <SingleCommit key={item?.node_id} name={item?.commit?.committer?.name} date={item?.commit?.committer?.date} message={item?.commit?.message} />;
+  });
 
   return (
     <div className="App">
       <AuthKeyForm />
-      <SingleCommit name="mamad" time={time} message="232kdsfbsdfj " />
+      {listOfCommits}
     </div>
   );
 }
