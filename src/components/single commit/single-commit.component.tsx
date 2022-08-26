1e4ff679da5cb5e3fdfcd7dda00253bb05ac3b00
Ali Hoseini <Ali4bcup@gmail.com>
2022-08-26 06:05:23 +0430

    Added React.memo to single commit

--- src/components/single commit/single-commit.component.tsx
+++ src/components/single commit/single-commit.component.tsx
@@ -1,24 +1,28 @@
+import React from "react";
 import Classes from "./single-commit.module.css";
 import { ISingleCommit } from "./single-commit.interface";
+
 
 function SingleCommit(props: ISingleCommit) {
   const { message, author, date } = props;
 
-  const dateObject = new Date(date)
+  const dateObject = new Date(date);
   const formattedDate = dateObject.toLocaleString("en-US", {
     month: "short",
     day: "numeric",
     hour: "numeric",
-    minute: "2-digit"
+    minute: "2-digit",
   });
 
-  return <div className={`${Classes.wrapper} ${Classes.shadow}`}>
-    <h3>{message}</h3>
-    <div className={Classes.details}>
+  return (
+    <div className={`${Classes.wrapper} ${Classes.shadow}`}>
+      <h3>{message}</h3>
+      <div className={Classes.details}>
         <p className={Classes.date}>{formattedDate}</p>
         <p>by {author}</p>
+      </div>
     </div>
-  </div>;
+  );
 }
 
-export default SingleCommit;
+export default React.memo(SingleCommit);
