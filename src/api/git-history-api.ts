9da446d95543d77ba08e9663bfdfd3610a1f3887
Ali Hoseini <Ali4bcup@gmail.com>
2022-08-26 04:43:06 +0430

    Added try catch to api call

--- src/api/git-history-api.ts
+++ src/api/git-history-api.ts
@@ -1,16 +1,20 @@
 import { Octokit } from "@octokit/core";
 
 const getGitHistory = async (authKey: string) => {
-  const octokit = new Octokit({
-    auth: authKey,
-  });
+  try {
+    const octokit = new Octokit({
+      auth: authKey,
+    });
 
-  const response = await octokit.request("GET /repos/ali4code/git-history/commits", {
-    owner: "OWNER",
-    repo: "REPO",
-  });
+    const response = await octokit.request("GET /repos/ali4code/git-history/commits", {
+      owner: "OWNER",
+      repo: "REPO",
+    });
 
-  return response.data;
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export default getGitHistory;
