dddad18dacd2ff034c5c7ca74090dfd3b684bc24
Ali Hoseini <Ali4bcup@gmail.com>
2022-08-26 06:14:16 +0430

    prevent event.default on add token

--- src/components/authkey-form/auth-key-form.component.tsx
+++ src/components/authkey-form/auth-key-form.component.tsx
@@ -1,19 +1,21 @@
 import { useState } from "react";
 import Button from "../ui/button";
-import Classes from './auth-key-form.module.css'
+import Classes from "./auth-key-form.module.css";
 function AuthKeyForm() {
   const [authKey, setAuthKey] = useState("");
-const addKey = () => {
-  if (authKey) {
-    localStorage.setItem('auth-key', authKey)
-    return
-  }
-  alert("please provide an Access Token")
-}
+  const addKey = (event: { preventDefault: () => void }) => {
+    event?.preventDefault();
+    if (authKey) {
+      localStorage.setItem("auth-key", authKey);
+      setAuthKey("")
+      return;
+    }
+    alert("please provide an Access Token");
+  };
   return (
     <form className={Classes.form} onSubmit={addKey}>
       <label htmlFor="auth-key">Enter Access Token</label>
-      <input className={Classes.input} type="text" id="auth-key" value={authKey} onChange={(e)=> setAuthKey(e.target.value)}/>
+      <input className={Classes.input} type="text" id="auth-key" value={authKey} onChange={(e) => setAuthKey(e.target.value)} />
       <Button type="submit">Add</Button>
     </form>
   );
